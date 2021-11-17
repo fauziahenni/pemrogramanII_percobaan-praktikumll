f50e95587c32337eadd53e4670e839bacc5cea75
fauziahenni <87329906+fauziahenni@users.noreply.github.com>
2021-11-17 09:21:37 -0800

    Add files via upload

---
+++ P2_1_1204013/book_1204013.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P2_1_1204013
+{
+    public class book_1204013 : Product_1204013
+    {
+        protected string pageCount;
+
+        public book_1204013(string Mytype, string title, string pagecount) : base(Mytype, title)
+        {
+            this.pageCount = pagecount;
+        }
+
+        public string PageCount
+        {
+            get
+            {
+                return pageCount;
+            }
+
+
+            set
+            {
+                pageCount = value;
+            }
+
+
+        }
+
+    }
+}
